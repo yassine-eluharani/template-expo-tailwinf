7b80c0ce36a87a77afcf17f84404ce5e9e2ee5aa
Yassine Eluharani <eluharani.yassine@gmail.com>
2024-03-29 12:24:16 +0000

    Template react native, with tailwind

---
+++ app/(tabs)/passTests.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <View className='flex-1 mt-[200]'>
+      <Text className='text-center text-2xl font-bold'>Test passed</Text>
+    </View>
+  )
+}
+
+export default Page
+
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -45,14 +44,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
